5bb9191884e68c394cda1ad620d4094a7581f6f2
ShauryaPandey456 <71887824+ShauryaPandey456@users.noreply.github.com>
2020-11-07 20:50:15 -0600

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,17 +1,18 @@
+#AKA water sprinkler board 
 import socket
 
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-# port number different for everyone 
-SERVER = "??????"
+SERVER = "192.168.195.1"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 def send(msg):
+    
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
@@ -19,6 +20,7 @@ def send(msg):
     client.send(send_length)
     client.send(message)
     print(client.recv(2048).decode(FORMAT))
+
 
 send("Hello World!")
 input()
